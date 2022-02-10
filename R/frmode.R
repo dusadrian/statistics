7dca70498dd007ee248e72339f2a94d56d7b35bc
Adrian Dusa <dusa.adrian@gmail.com>
2022-02-10 12:38:48 +0200

    back to version 0.5

--- R/frmode.R
+++ R/frmode.R
@@ -1,19 +1,14 @@
 `frmode` <- function(x) {
     tbl <- table(x)
     wm <- which.max(tbl)
+    
     fmode <- names(tbl[wm])
     if (admisc::possibleNumeric(fmode)) {
         fmode <- admisc::asNumeric(fmode)
     }
+
     if (length(tbl[tbl == wm]) > 1) {
         message("Multiple modes detected, only the first is returned.")
     }
     return(fmode)
-}
-
-
-
-`fmode` <- function(...) {
-    .Deprecated(msg = "Function fmode() is deprecated, and has been renamed to frmode()\n")
-    fmode(...)
 }
--- R/t_testAB.R
+++ R/t_testAB.R
@@ -3,33 +3,51 @@ function(x, y = NULL,
          alternative = c("two.sided", "less", "greater"), var.equal = FALSE,
          mu = 0, paired = FALSE, conf.level = 0.95, data) {
     
-    cautare <- unique(c(match(alternative, c("!=", "<", ">")),
+    pmatching <- unique(c(match(alternative, c("!=", "<", ">")),
                         match(alternative, c("two.sided", "less", "greater"))))
-    cautare <- cautare[!is.na(cautare)]
-    alternative <- c("two.sided", "less", "greater")[cautare]
+    pmatching <- pmatching[!is.na(pmatching)]
+    alternative <- c("two.sided", "less", "greater")[pmatching]
     
     if (!paired) {
-        omogenitate <- c("are not equal", "are equal")
+        homogeneity <- c("are not equal", "are equal")
         
         if (is.null(y)) {
-            omog.test <- ansari.test(x, data=data, exact=FALSE)
-            }
-        else {
-            omog.test <- ansari.test(x, y, exact=FALSE)
-            }
-        
-        p.value <- omog.test$p.value
-        var.equal <- ifelse(p.value > 0.05, TRUE, FALSE)
-        cat ("\nThe Ansari-Bradley test for the homogeneity of variances has a value of\n",
-             paste("p=", round(p.value, 4), sep=""), " therefore the variances are ",
-             omogenitate[var.equal + 1], "\n", sep="")
+            homogtest <- ansari.test(x, data = data, exact = FALSE)
         }
+        else {
+            homogtest <- ansari.test(x, y, exact = FALSE)
+        }
+        
+        p.value <- homogtest$p.value
+        var.equal <- ifelse(p.value > (1 - conf.level), TRUE, FALSE)
+        cat (
+            "\nThe Ansari-Bradley test for the homogeneity of variances has a value of\n",
+            paste("p =", round(p.value, 4), sep = ""),
+            " therefore the variances are ",
+            homogeneity[var.equal + 1],
+            "\n",
+            sep = ""
+        )
+    }
     
     if (is.null(y)) {
-        test <- t.test(x, alternative=alternative, conf.level=conf.level, var.equal=var.equal, paired=paired, data=data)
+        test <- t.test(
+            x,
+            alternative = alternative,
+            conf.level = conf.level,
+            var.equal = var.equal,
+            paired = paired, data = data
+        )
         }
     else {
-        test <- t.test(x, y, alternative=alternative, conf.level=conf.level, paired=paired, var.equal=var.equal)
+        test <- t.test(
+            x,
+            y,
+            alternative = alternative,
+            conf.level = conf.level,
+            paired = paired,
+            var.equal = var.equal
+        )
         }
         
     print(test)
@@ -38,6 +56,6 @@ function(x, y = NULL,
         return(invisible(list("t.test" = test)))
     }
     else {
-        return(invisible(list("Homogeneity of variances" = omog.test, "t.test" = test)))
+        return(invisible(list("Homogeneity of variances" = homogtest, "t.test" = test)))
     }
 }
