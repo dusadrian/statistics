70e929d406cbebab9b7925af72c45e6ac0501a48
Adrian Dușa <dusa.adrian@gmail.com>
2024-07-27 00:51:06 +0300

    attempt to circumvent the "cannot use 'paired' in formula method" error

--- R/t_testAB.R
+++ R/t_testAB.R
@@ -1,8 +1,9 @@
-`t_testAB` <-
-function(x, y = NULL,
-         alternative = c("two.sided", "less", "greater"), var.equal = FALSE,
-         mu = 0, paired = FALSE, conf.level = 0.95, data) {
-    
+`t_testAB` <- function(
+    x, y = NULL,
+    alternative = c("two.sided", "less", "greater"), var.equal = FALSE,
+    mu = 0, paired = FALSE, conf.level = 0.95, data = NULL
+) {
+
     if (identical(alternative, c("two.sided", "less", "greater"))) {
         alternative <- "two.sided"
     }
@@ -21,19 +22,19 @@ function(x, y = NULL,
     }
 
     alternative <- c("two.sided", "less", "greater")[pmatching]
-    
+
     if (!paired) {
         homogeneity <- c("not equal", "equal")
-        
+
         if (is.null(y)) {
             homogtest <- ansari.test(x, data = data, exact = FALSE)
         }
         else {
             homogtest <- ansari.test(x, y, exact = FALSE)
         }
-        
+
         p.value <- homogtest$p.value
-        var.equal <- ifelse(p.value > (1 - conf.level), TRUE, FALSE)
+        var.equal <- p.value > (1 - conf.level)
         cat (
             "\nThe Ansari-Bradley test for the homogeneity of variances has a value of",
             paste("p = ", round(p.value, 4), sep = ""),
@@ -43,17 +44,23 @@ function(x, y = NULL,
             sep = ""
         )
     }
-    
+
     if (is.null(y)) {
-        test <- t.test(
-            x,
+        callist <- list(
+            formula = x,
             alternative = alternative,
             conf.level = conf.level,
             var.equal = var.equal,
-            paired = paired, data = data
+            data = data
         )
+
+        if (paired) {
+            callist$paired <- TRUE
         }
-    else {
+
+        test <- do.call("t.test", callist)
+
+    } else {
         test <- t.test(
             x,
             y,
@@ -62,10 +69,10 @@ function(x, y = NULL,
             paired = paired,
             var.equal = var.equal
         )
-        }
-        
+    }
+
     print(test)
-    
+
     if (paired) {
         return(invisible(list("t.test" = test)))
     }
