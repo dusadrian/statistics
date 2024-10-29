9e495ea95a73e0f0d4460be3c86a91afafccb5b7
Adrian Dușa <dusa.adrian@gmail.com>
2024-10-29 20:30:04 +0200

    more plot space

--- R/daria.R
+++ R/daria.R
@@ -1,6 +1,6 @@
 `daria` <-
 function (area, z1, z2, draw = FALSE) {
-    
+
     if (missing(area) | is.numeric(area)) {
         cat("\n")
         admisc::stopError("Unspecified area.")
@@ -8,49 +8,61 @@ function (area, z1, z2, draw = FALSE) {
         cat("\n")
         cat("The area of an exact value, out of an infinity of other values, is\ninfinitely small, practically equal to zero.")
         return()
-    } else if (!(area %in% c("r", "o", "a", "right", "over", "above",
-                             "l", "u", "left", "under",
-                             "b", "between"))) {
+    } else if (
+        !is.element(
+            area,
+            c("r", "o", "a", "right", "over", "above", "l", "u", "left", "under", "b", "between")
+        )
+    ) {
         admisc::stopError("The specified area is incorrect.")
     }
-    
+
     if (missing(z1)) {
         admisc::stopError("The value of z was not specified.")
         } else {
             if (z1 > 0) {
-                zet1 <- paste("+", as.character(round(z1, digits = 3)), sep = "") 
+                zet1 <- paste("+", as.character(round(z1, digits = 3)), sep = "")
             } else {
                 zet1 <- as.character(round(z1, digits = 3))
             }
         }
-    
-    if (area %in% c("b", "between") & !missing(z1) & missing(z2)) {
+
+    if (is.element(area, c("b", "between")) & !missing(z1) & missing(z2)) {
         admisc::stopError("The second z value was not specified.")
         }
-    
+
     if (!missing(z2)) {
         if (z2 > 0) {
-            zet2 <- paste("+", as.character(round(z2, digits = 3)), sep = "") 
+            zet2 <- paste("+", as.character(round(z2, digits = 3)), sep = "")
         } else {
             zet2 <- as.character(round(z2, digits = 3))
         }
     }
-    
+
     if (draw) {
         if (dev.cur() == 1) {
             dev.new(width = 30/2.54, height = 21/2.54)
         }
+
+        par(mar = c(1, 0, 0, 0))
+
+        x <- seq(-4, 4, length.out = 1000)
+        y <- dnorm(x)
+
         plot(
-            seq(-4, 4, length = 1000),
-            seq(0, 0.4, length = 1000),
-            type = "n",
-            xlab = "",
+            x,
+            y,
+            type = "l",
+            xaxt = "n",
+            yaxt = "n",
             ylab = "",
-            axes = FALSE
+            xlab = "",
+            ylim = c(0, max(y)),
+            bty = "n"
         )
     }
-    
-    if (area %in% c("r", "o", "a", "right", "over", "above")) {
+
+    if (is.element(area, c("r", "o", "a", "right", "over", "above"))) {
         area <- 1 - pnorm(z1)
         if (abs(z1) > 4) {z1 <- 4*sign(z1)}
         xdreapta <- seq(z1, 4, 0.001)
@@ -63,13 +75,8 @@ function (area, z1, z2, draw = FALSE) {
                 col = "#79a74c"
             )
             segments(z1, 0, z1, dnorm(z1))
-            # text(rep(-2.8, 4), c(0.3, 0.28, 0.26, 0.24),
-            #     c(paste("The area to the right of", paste("z=", zet1, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or",
-            #       paste(round(area, 3)*100,"%", sep = "")),
-            #       col=rep(c("black", "#cb2626"), 2))
             }
-    } else if (area %in% c("u", "l", "under", "left")) {
+    } else if (is.element(area, c("u", "l", "under", "left"))) {
         area <- pnorm(z1)
         if (abs(z1) > 4) {
             z1 <- 4*sign(z1)
@@ -77,7 +84,7 @@ function (area, z1, z2, draw = FALSE) {
 
         xstanga <- seq(-4, z1, 0.001)
         ystanga <- dnorm(xstanga)
-        
+
         if (draw) {
             polygon(
                 c(xstanga, rev(xstanga)),
@@ -87,26 +94,21 @@ function (area, z1, z2, draw = FALSE) {
             )
 
             segments(z1, 0, z1, dnorm(z1))
-            # text(rep(-2.8, 4), c(0.3, 0.28, 0.26, 0.24),
-            #     c(paste("The area to the left of", paste("z=", zet1, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or",
-            #       paste(round(area, 3)*100, "%", sep = "")),
-            #       col=rep(c("black", "#cb2626"), 2))
         }
-    } else if (area %in% c("b", "between")) {
+    } else if (is.element(area, c("b", "between"))) {
         if (z1 > z2) {
             sz <- z1; z1 <- z2; z2 <- sz
         }
-        
+
         area <- pnorm(z2) - pnorm(z1)
         if (abs(z1) > 4) {
             z1 <- 4*sign(z1)
         }
-        
+
         if (abs(z2) > 4) {
             z2 <- 4*sign(z2)
         }
-        
+
         xintre <- seq(z1, z2, 0.001)
         yintre <- dnorm(xintre)
         if (draw) {
@@ -119,30 +121,31 @@ function (area, z1, z2, draw = FALSE) {
 
             segments(z1, 0, z1, dnorm(z1))
             segments(z2, 0, z2, dnorm(z2))
-            # text(rep(-2.8, 4), c(0.3, 0.28, 0.26, 0.24),
-            #     c(paste("The area between", paste("z=", zet1, sep = ""),
-            #       "and", paste("z=", zet2, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or",
-            #       paste(round(area, 3)*100, "%", sep = "")),
-            #       col=rep(c("black", "#cb2626"), 2))
         }
     }
-    
+
     if (draw) {
-        eval(
-            parse(
-                text = "curve(dnorm(x, mean=0, sd=1), from=-4, to=4, las=1, add=TRUE)"
-            )
+
+        RStudio <- identical(names(dev.cur()), "RStudioGD")
+        Positron <- grepl("Positron", names(dev.cur()))
+
+        tick_length <- ifelse(Positron|RStudio, 0.02, 0.015)
+        label_offset <- ifelse (Positron|RStudio, 0.1, -1.2)
+
+
+        ticklabels <- c(-4:0, paste("+", as.character(1:4), sep = ""))
+
+        axis(
+            side = 1,
+            at = seq(-4, 4, by = 1),
+            labels = TRUE,
+            cex.axis = ifelse(Positron|RStudio, 0.8, 1),
+            lwd = 0,
+            line = label_offset
         )
-        
+
+        segments(-4:4, y0 = 0, x1 = -4:4, y1 = -tick_length * (par("usr")[4] - par("usr")[3]))
         segments(-4, 0, 4, 0)
-        segments(-4:4, rep(0, 9), -4:4, rep(-0.004, 9))
-        text(
-            -4:4,
-            rep(-0.01, 9),
-            c(-4:0, paste("+", as.character(1:4), sep = "")),
-            cex = 0.8
-        )
     }
 
     return(area)
--- R/dariat.R
+++ R/dariat.R
@@ -1,6 +1,6 @@
 `dariat` <-
-function (area, t1, t2, df, draw=FALSE) {
-    
+function (area, t1, t2, df, draw = FALSE) {
+
     if (missing(area) | is.numeric(area)) {
         admisc::stopError("Unspecified area.")
     } else if (area == "exact" | area == "e") {
@@ -8,50 +8,67 @@ function (area, t1, t2, df, draw=FALSE) {
         cat("The area of an exact value, out of an infinity of other\nvalues,",
             "is infinitely small, practically equal to zero.")
         return()
-    } else if (!(area %in% c("r", "o", "right", "over",
-                             "l", "u", "left", "under",
-                             "b", "between"))) {
+    } else if (
+        !is.element(
+            area,
+            c("r", "o", "a", "right", "over", "above", "l", "u", "left", "under", "b", "between")
+        )
+    ) {
         admisc::stopError("The specified area is incorrect.")
     }
-    
+
     if (missing(t1)) {
         admisc::stopError("The value of t was not specified.")
         } else {
             if (t1 > 0) {
-                te1 <- paste("+", as.character(round(t1, digits = 3)), sep = "") 
+                te1 <- paste("+", as.character(round(t1, digits = 3)), sep = "")
             } else {
                 te1 <- as.character(round(t1, digits = 3))
             }
         }
-    
+
     if (area %in% c("b", "between") & !missing(t2) & missing(t2)) {
         admisc::stopError("The second t value was not specified.")
         }
-    
+
     if (!missing(t2)) {
         if (t2 > 0) {
-            te2 <- paste("+", as.character(round(t2, digits = 3)), sep = "") 
+            te2 <- paste("+", as.character(round(t2, digits = 3)), sep = "")
         } else {
             te2 <- as.character(round(t2, digits = 3))
         }
     }
-    
+
     if (missing(df)) {
         admisc::stopError("The degrees of freedom were not specified.")
     }
-    
+
     limita <- 4 + ifelse(df < 21, 1, 0)
-    
+
     if (draw) {
         if (dev.cur() == 1) {
-            dev.new(width=30/2.54, height=21/2.54)
+            dev.new(width = 30/2.54, height = 21/2.54)
         }
-        plot(seq(-limita, limita, length=1000),
-             seq(0, 0.4, length=1000),
-             type="n", xlab="", ylab="", axes=FALSE)
+
+        par(mar = c(1, 0, 0, 0))
+
+        x <- seq(-4, 4, length.out = 1000)
+        y <- dt(x, df)
+
+        plot(
+            x,
+            y,
+            type = "l",
+            xaxt = "n",
+            yaxt = "n",
+            ylab = "",
+            xlab = "",
+            ylim = c(0, max(y)),
+            bty = "n"
+        )
     }
-    
-    if (area %in% c("r", "o", "a", "right", "over", "above")) {
+
+    if (is.element(area, c("r", "o", "a", "right", "over", "above"))) {
         area <- 1 - pt(t1, df)
         if (abs(t1) > limita) {t1 <- limita*sign(t1)}
         xdreapta <- seq(t1, limita, 0.001)
@@ -61,14 +78,8 @@ function (area, t1, t2, df, draw=FALSE) {
                     c(rep(0, length(xdreapta)), rev(ydreapta)),
                     border=NA, col="#79a74c")
             segments(t1, 0, t1, dt(t1, df))
-            # text(rep(-3.2 + 0.5*(limita == 4), 5), seq(0.32, 0.24, -0.02),
-            #     c(paste("At", df, "degrees of freedom,"), 
-            #       paste("the area to the right of", paste("t=", te1, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or", 
-            #       paste(round(area, 3)*100,"%", sep = "")),
-            #       col=c("black", rep(c("black", "#cb2626"), 2)))
         }
-    } else if (area %in% c("l", "u", "left", "under")) {
+    } else if (is.element(area, c("u", "l", "under", "left"))) {
         area <- pt(t1, df)
         if (abs(t1) > limita) {t1 <- limita*sign(t1)}
         xstanga <- seq(-limita, t1, 0.001)
@@ -78,15 +89,8 @@ function (area, t1, t2, df, draw=FALSE) {
                     c(rep(0, length(xstanga)), rev(ystanga)),
                     border=NA, col="#79a74c" )
             segments(t1, 0, t1, dt(t1, df))
-            # text(rep(-3.2 + 0.5*(limita == 4), 5), seq(0.32, 0.24, -0.02),
-            #     c(paste("At", df, "degrees of freedom,"), 
-            #       paste("the area to the left of", paste("t=", te1, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or", 
-            #       paste(round(area, 3)*100, "%", sep = "")),
-            #       col=c("black", rep(c("black", "#cb2626"), 2)))
-            # curve(dt(x, df), from=-limita, to=limita, las=1, add=TRUE)
         }
-    } else if (area %in% c("b", "between")) {
+    } else if (is.element(area, c("b", "between"))) {
         if (t1 > t2) {temp <- t1; t1 <- t2; t2 <- temp}
         area <- pt(t2, df) - pt(t1, df)
         if (abs(t1) > limita) {t1 <- limita*sign(t1)}
@@ -94,29 +98,35 @@ function (area, t1, t2, df, draw=FALSE) {
         xintre <- seq(t1, t2, 0.001)
         yintre <- dt(xintre, df)
         if (draw) {
-            polygon(c(xintre, rev(xintre)), 
+            polygon(c(xintre, rev(xintre)),
                     c(rep(0, length(xintre)), rev(yintre)),
                     border=NA, col="#79a74c" )
             segments(c(t1, t2), 0, c(t1, t2), dt(c(t1, t2), df))
-            # text(rep(-3.2 + 0.5*(limita == 4), 5), seq(0.32, 0.24, -0.02),
-            #     c(paste("At", df, "degrees of freedom,"), 
-            #       paste("the area between", paste("t=", te1, sep = ""),
-            #       "and", paste("t=", te2, sep = ""),
-            #       "has:", sep=" "), round(area, 3), "or",
-            #       paste(round(area, 3)*100, "%", sep = "")),
-            #       col=c("black", rep(c("black", "#cb2626"), 2)))
         }
     }
-    
+
     if (draw) {
-        command <- paste("curve(dt(x, df), from=-", limita, ", to=", limita, ", las=1, add=TRUE)", sep = "")
-        eval(parse(text = command))
-        segments(-limita, 0, limita, 0)
-        segments(-limita:limita, rep(0, limita*2 + 1),
-                 -limita:limita, rep(-0.004, limita*2 + 1))
-        text(-limita:limita, rep(-0.01, limita*2 + 1),
-             c(-limita:0, paste("+", as.character(1:limita), sep = "")),
-             cex=0.8)
+
+        RStudio <- identical(names(dev.cur()), "RStudioGD")
+        Positron <- grepl("Positron", names(dev.cur()))
+
+        tick_length <- ifelse(Positron|RStudio, 0.02, 0.015)
+        label_offset <- ifelse (Positron|RStudio, 0.1, -1.2)
+
+
+        ticklabels <- c(-4:0, paste("+", as.character(1:4), sep = ""))
+
+        axis(
+            side = 1,
+            at = seq(-4, 4, by = 1),
+            labels = TRUE,
+            cex.axis = ifelse(Positron|RStudio, 0.8, 1),
+            lwd = 0,
+            line = label_offset
+        )
+
+        segments(-4:4, y0 = 0, x1 = -4:4, y1 = -tick_length * (par("usr")[4] - par("usr")[3]))
+        segments(-4, 0, 4, 0)
     }
     return(area)
 }
